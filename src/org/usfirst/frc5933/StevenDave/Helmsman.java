04f84126df9a308756f4e878d3615d9ff150b050
Andrew Van Uitert <avanuitert@control4.com>
2016-03-07 08:58:50 -0700

    Adding the missing Digital Output to the RobotoBuilder yaml file. Removed the rear ultrsonic form the yaml. Added utlrasonic drive straight. Cleaned up the instantiation of the Digital output trigger. Other misc fun with projects.

--- src/org/usfirst/frc5933/StevenDave/Helmsman.java
+++ src/org/usfirst/frc5933/StevenDave/Helmsman.java
@@ -12,119 +12,116 @@ import edu.wpi.first.wpilibj.DigitalOutput;
 
 public class Helmsman {
 
-    private AnalogGyro gyro_ = RobotMap.sensorsAnalogGyro;
-    private double beginingGyroAngle_ = 0;
-    private double beginingGyroCenter_ = 0;
-    private double beginingGyroOffset_ = 0;
-    private double initialGyroAngle_ = 0;
-    private double initialGyroCenter_ = 0;
-    private double initialGyroOffset_ = 0;
-    
-    private FieldOfPlay field_ = new FieldOfPlay();
-    private BuiltInAccelerometer accel_ = new BuiltInAccelerometer();
-    private static CameraServer cameraServer_;
+	private AnalogGyro gyro_ = RobotMap.sensorsAnalogGyro;
+	private double beginingGyroAngle_ = 0;
+	private double beginingGyroCenter_ = 0;
+	private double beginingGyroOffset_ = 0;
+	private double initialGyroAngle_ = 0;
+	private double initialGyroCenter_ = 0;
+	private double initialGyroOffset_ = 0;
 
-    private CANTalon frontLeftMotor_ = RobotMap.driveTrainFrontLeftMotor;
-    private double initialFrontLeftMotorPosition_ = 0;
-    private double currentFrontLeftMotorPosition_ = 0;
-    
-    private CANTalon frontRightMotor_ = RobotMap.driveTrainFrontRightMotor;
-    private double initialFrontRightMotorPosition_ = 0;
-    private double currentFrontRightMotorPosition_ = 0;
+	private FieldOfPlay field_ = new FieldOfPlay();
+	private BuiltInAccelerometer accel_ = new BuiltInAccelerometer();
+	private static CameraServer cameraServer_;
 
-    private static final double FORWARD_V2D = 85.7;     // Voltage to distance (inches) scalar for forward ultrasonic
-    private static final double PORT_V2D = 85.7;     // Voltage to distance (inches) scalar for port ultrasonic
-    private static final double STARBOARD_V2D = 85.7;     // Voltage to distance (inches) scalar for starboard ultrasonic
+	private CANTalon frontLeftMotor_ = RobotMap.driveTrainFrontLeftMotor;
+	private double initialFrontLeftMotorPosition_ = 0;
+	private double currentFrontLeftMotorPosition_ = 0;
 
-    private static AnalogInput forwardUltrasonic_ = RobotMap.sensorsForwardUltrasonic; //bow
-    private static AnalogInput portUltrasonic_ = RobotMap.sensorsPortUltrasonic; //left
-    private static AnalogInput starboardUltrasonic_ = RobotMap.sensorsStarboardUltrasonic; //right
+	private CANTalon frontRightMotor_ = RobotMap.driveTrainFrontRightMotor;
+	private double initialFrontRightMotorPosition_ = 0;
+	private double currentFrontRightMotorPosition_ = 0;
 
-    Helmsman() {
-        gyro_.initGyro();
-        beginingGyroAngle_ = gyro_.getAngle();
-        beginingGyroCenter_ = gyro_.getCenter();
-        beginingGyroOffset_ = gyro_.getOffset();
-        cameraServer_ = CameraServer.getInstance();
-        cameraServer_.setQuality(100);
-        cameraServer_.setSize(0);
-        cameraServer_.startAutomaticCapture("cam0");
-        DigitalOutput ultrasonicTrigger_ = new DigitalOutput(1);
-        ultrasonicTrigger_.enablePWM(0.25);
-        ultrasonicTrigger_.setPWMRate(5);
-    }
-    
-    public void resetGyro() {
-        gyro_.reset();
-    }
+	private static final double FORWARD_V2D = 85.7;     // Voltage to distance (inches) scalar for forward ultrasonic
+	private static final double PORT_V2D = 85.7;     // Voltage to distance (inches) scalar for port ultrasonic
+	private static final double STARBOARD_V2D = 85.7;     // Voltage to distance (inches) scalar for starboard ultrasonic
 
-    public final double getBeginingGyroAngle() {
-        return beginingGyroAngle_;
-    }
+	private static AnalogInput forwardUltrasonic_ = RobotMap.sensorsForwardUltrasonic; //bow
+	private static AnalogInput portUltrasonic_ = RobotMap.sensorsPortUltrasonic; //left
+	private static AnalogInput starboardUltrasonic_ = RobotMap.sensorsStarboardUltrasonic; //right
 
-    public final double getBeginingGyroOffset() {
-        return beginingGyroOffset_;
-    }
+	Helmsman() {
+		gyro_.initGyro();
+		beginingGyroAngle_ = gyro_.getAngle();
+		beginingGyroCenter_ = gyro_.getCenter();
+		beginingGyroOffset_ = gyro_.getOffset();
+		cameraServer_ = CameraServer.getInstance();
+		cameraServer_.setQuality(100);
+		cameraServer_.setSize(0);
+		cameraServer_.startAutomaticCapture("cam0");
+	}
 
-    public final double getBeginingGyroCenter() {
-        return beginingGyroCenter_;
-    }
+	public void resetGyro() {
+		gyro_.reset();
+	}
 
-    public final double getInitialGyroAngle() {
-        return initialGyroAngle_;
-    }
+	public final double getBeginingGyroAngle() {
+		return beginingGyroAngle_;
+	}
 
-    public final double getInitialGyroOffset() {
-        return initialGyroOffset_;
-    }
+	public final double getBeginingGyroOffset() {
+		return beginingGyroOffset_;
+	}
 
-    public final double getInitialGyroCenter() {
-        return initialGyroCenter_;
-    }
+	public final double getBeginingGyroCenter() {
+		return beginingGyroCenter_;
+	}
 
-    public final double getCurrentGyroAngle() {
-        return gyro_.getAngle();
-    }
+	public final double getInitialGyroAngle() {
+		return initialGyroAngle_;
+	}
 
-    public final double getForwardUltrasonicDistance() {
-        return forwardUltrasonic_.getAverageVoltage() * FORWARD_V2D;
-    }
-    
-    public final double getPortUltrasonicDistance() {
-        return portUltrasonic_.getAverageVoltage() * PORT_V2D;
-    }
-    
-    public final double getStarboardUltrasonicDistance() {
-        return starboardUltrasonic_.getAverageVoltage() * STARBOARD_V2D;
-    }
+	public final double getInitialGyroOffset() {
+		return initialGyroOffset_;
+	}
 
-    public void initTracking() {
-        gyro_.reset();
-        initialGyroAngle_ = gyro_.getAngle();
-        initialGyroCenter_ = gyro_.getCenter();
-        initialGyroOffset_ = gyro_.getOffset();
+	public final double getInitialGyroCenter() {
+		return initialGyroCenter_;
+	}
 
-        initialFrontLeftMotorPosition_ = frontLeftMotor_.get();
-        currentFrontLeftMotorPosition_ = initialFrontLeftMotorPosition_;
+	public final double getCurrentGyroAngle() {
+		return gyro_.getAngle();
+	}
 
-        initialFrontRightMotorPosition_ = frontRightMotor_.get();
-        currentFrontRightMotorPosition_ = initialFrontRightMotorPosition_;
-    }
-    
-    public void trackPosition() {
-        currentFrontLeftMotorPosition_ = frontLeftMotor_.get();
-        currentFrontRightMotorPosition_ = frontRightMotor_.get();
-    }
+	public final double getForwardUltrasonicDistance() {
+		return forwardUltrasonic_.getAverageVoltage() * FORWARD_V2D;
+	}
 
-    public final double getAcceleromoterZ() {
-        return accel_.getZ();
-    }
+	public final double getPortUltrasonicDistance() {
+		return portUltrasonic_.getAverageVoltage() * PORT_V2D;
+	}
 
-    public final double getCurrentLeftMotorPosition() {
-        return currentFrontLeftMotorPosition_;
-    }
-    
-    public final double getCurrentRightMotorPosition() {
-        return currentFrontRightMotorPosition_;
-    }
+	public final double getStarboardUltrasonicDistance() {
+		return starboardUltrasonic_.getAverageVoltage() * STARBOARD_V2D;
+	}
+
+	public void initTracking() {
+		gyro_.reset();
+		initialGyroAngle_ = gyro_.getAngle();
+		initialGyroCenter_ = gyro_.getCenter();
+		initialGyroOffset_ = gyro_.getOffset();
+
+		initialFrontLeftMotorPosition_ = frontLeftMotor_.get();
+		currentFrontLeftMotorPosition_ = initialFrontLeftMotorPosition_;
+
+		initialFrontRightMotorPosition_ = frontRightMotor_.get();
+		currentFrontRightMotorPosition_ = initialFrontRightMotorPosition_;
+	}
+
+	public void trackPosition() {
+		currentFrontLeftMotorPosition_ = frontLeftMotor_.get();
+		currentFrontRightMotorPosition_ = frontRightMotor_.get();
+	}
+
+	public final double getAcceleromoterZ() {
+		return accel_.getZ();
+	}
+
+	public final double getCurrentLeftMotorPosition() {
+		return currentFrontLeftMotorPosition_;
+	}
+
+	public final double getCurrentRightMotorPosition() {
+		return currentFrontRightMotorPosition_;
+	}
 }
--- src/org/usfirst/frc5933/StevenDave/mapping/Rectangle.java
+++ src/org/usfirst/frc5933/StevenDave/mapping/Rectangle.java
@@ -1,36 +1,36 @@
 package org.usfirst.frc5933.StevenDave.mapping;
 
 public class Rectangle extends Shape {
-    protected double width_ = 0;
-    protected double length_ = 0;
+	protected double width_ = 0;
+	protected double length_ = 0;
 
-    public Rectangle() {
-        super();
-    }
+	public Rectangle() {
+		super();
+	}
 
-    public Rectangle(String name) {
-        super(name);
-    }
+	public Rectangle(String name) {
+		super(name);
+	}
 
-    public Rectangle(String name, Coordinate origin) {
-        super(name, origin);
-    }
+	public Rectangle(String name, Coordinate origin) {
+		super(name, origin);
+	}
 
-    public Rectangle(Coordinate origin) {
-        super(origin);
-    }
+	public Rectangle(Coordinate origin) {
+		super(origin);
+	}
 
-    public Rectangle(String name, Coordinate origin, double width, double length) {
-        super(name, origin);
-        width_ = width;
-        length_ = length;
-    }
+	public Rectangle(String name, Coordinate origin, double width, double length) {
+		super(name, origin);
+		width_ = width;
+		length_ = length;
+	}
 
-    public final double getWidth() {
-        return width_;
-    }
+	public final double getWidth() {
+		return width_;
+	}
 
-    public final double getLength() {
-        return width_;
-    }
+	public final double getLength() {
+		return width_;
+	}
 }
